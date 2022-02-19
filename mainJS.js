f2917a53ef05943b439c10bd367118eb60886b5d
ignasc <ignui.c@gmail.com>
2022-02-19 12:01:58 +0000

    Initial template created

---
+++ mainJS.js
@@ -0,0 +1,24 @@
+/*
+Load dataset from
+Kickstarter Pledges: https://cdn.freecodecamp.org/testable-projects-fcc/data/tree_map/kickstarter-funding-data.json
+Movie Sales: https://cdn.freecodecamp.org/testable-projects-fcc/data/tree_map/movie-data.json
+Video Game Sales: https://cdn.freecodecamp.org/testable-projects-fcc/data/tree_map/video-game-sales-data.json
+/*
+
+/*LOCAL DATA FOR DEVELOPMENT*/
+
+const JSON_KICKSTARTER = ".\data\kickstarter-funding-data.json";
+const JSON_MOVIE = ".\data\movie-data.json";
+const JSON_GAMES = ".\data\video-game-sales-data.json";
+       
+/*Set up SVG canvas to work on*/
+const pageWidth  = document.documentElement.scrollWidth;
+const chartWidth = 1024;
+const chartHeight = 800;
+const padding = 60;
+
+/*LEARN TO USE PROMISE.ALL TO LOAD ALL DATA*/
+
+function main(){
+
+};
